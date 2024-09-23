dd3760752e2b66ac8e97f2d5511330017491e4b2
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-23 21:00:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖Machine learning app')
 
 st.info('This app deploys a machine learning model')
+
+df = read.csv('https://raw.githubusercontent.com/Lo-K-ee/datasets/refs/heads/main/penguins_cleaned.csv')
+df
