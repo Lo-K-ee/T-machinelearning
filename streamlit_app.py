fc5e91abb7302a08d6fe05f9dde52e34d8623997
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-24 13:29:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title('🤖Machine learning app')
 
-st.info('This app deploys a linear regression model for a simple cleaned dataset')
+st.info('This app builds a machine learning model for a simple cleaned dataset')
 
 with st.expander('Raw Cleaned Data'):
   df = pd.read_csv('https://raw.githubusercontent.com/Lo-K-ee/datasets/refs/heads/main/penguins_cleaned.csv')
@@ -17,6 +17,8 @@ with st.expander('Target and predictors'):
   st.write('**y**')
   y = df.species
   y
-  
 
+  # species	island	bill_length_mm	bill_depth_mm	flipper_length_mm	body_mass_g	sex
+  
+  st.scatter_chart(df, bill_length_mm, body_mass_g)
 
