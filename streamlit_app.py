e0ec63ae54f84b38eba66801a3fc9a296bbd381b
Lokesh Palaniraj <130686597+Lo-K-ee@users.noreply.github.com>
2024-09-24 13:16:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,14 @@ with st.expander('Raw Cleaned Data'):
   df = pd.read_csv('https://raw.githubusercontent.com/Lo-K-ee/datasets/refs/heads/main/penguins_cleaned.csv')
   df
 
-st.write('Here, we have displayed both the dependant variable and the independant variables seperately')
+st.write('*Here, we have displayed both the dependant variable and the independant variables seperately*')
+with st.expander('Target and predictors'):
+  st.write('**X**')
+  X = df.drop('species')
+  X
+  st.write('**y**')
+  y = df.species
+  y
+  
 
 
